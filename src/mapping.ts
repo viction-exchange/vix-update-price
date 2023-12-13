efe78d1d9f14c75ac6bb3dae8058fa6186585c11
Thanh Luan <thanhluan.bkhn@gmail.com>
2023-12-13 18:59:00 +0700

    init

---
+++ src/mapping.ts
@@ -0,0 +1,38 @@
+import {
+  NewPriceFeed as NewPriceFeedEvent,
+  PriceUpdate as PriceUpdateEvent
+} from "../generated/PythPriceUpdater/PythPriceUpdater"
+import {
+  NewPriceFeed,
+  PriceUpdate
+} from "../generated/schema"
+
+export function handleNewPriceFeed(event: NewPriceFeedEvent): void {
+  let entity = new NewPriceFeed(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.token = event.params.token
+  entity.priceId = event.params.priceId
+  entity.priceFeed = event.params.priceFeed
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePriceUpdate(event: PriceUpdateEvent): void {
+  let entity = new PriceUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.token = event.params.token
+  entity.price = event.params.price
+  entity.priceFeed = event.params.priceFeed
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
